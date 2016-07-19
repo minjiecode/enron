2baedeb0d1dba825274e505f496725760be80872
Min Jie <minjiecoder@gmail.com>
2016-07-19 09:00:06 +0800

    Update

--- poi_id.py
+++ poi_id.py
@@ -179,7 +179,68 @@ def al_tuning(features_list):
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Chosen classifier
-clf = tree.DecisionTreeClassifier(min_samples_split=8,criterion= 'entropy')
+clf = tree.DecisionTreeClassifier(min_samples_split=4,criterion= 'entropy')
+# Cross Validation
+def cross_validation(clf, dataset, feature_list, folds = 1000):
+	"""Cross validate the algorithm using StratifiedShuffleSplit"""
+	data = featureFormat(dataset, feature_list, sort_keys = True)
+	labels, features = targetFeatureSplit(data)
+	cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+	true_negatives = 0
+	false_negatives = 0
+	true_positives = 0
+	false_positives = 0
+	for train_idx, test_idx in cv: 
+	    features_train = []
+	    features_test  = []
+	    labels_train   = []
+	    labels_test    = []
+	    for ii in train_idx:
+	        features_train.append( features[ii] )
+	        labels_train.append( labels[ii] )
+	    for jj in test_idx:
+	        features_test.append( features[jj] )
+	        labels_test.append( labels[jj] )
+
+	     ### fit the classifier using training set, and test on test set
+	    clf.fit(features_train, labels_train)
+	    predictions = clf.predict(features_test)
+	    for prediction, truth in zip(predictions, labels_test):
+	        if prediction == 0 and truth == 0:
+	            true_negatives += 1
+	        elif prediction == 0 and truth == 1:
+	            false_negatives += 1
+	        elif prediction == 1 and truth == 0:
+	            false_positives += 1
+	        elif prediction == 1 and truth == 1:
+	            true_positives += 1
+	        else:
+	            print "Warning: Found a predicted label not == 0 or 1."
+	            print "All predictions should take value 0 or 1."
+	            print "Evaluating performance for processed predictions:"
+	            break
+
+	try:
+	    total_predictions = true_negatives + false_negatives \
+	    					+ false_positives + true_positives
+	    accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+	    precision = 1.0*true_positives/(true_positives+false_positives)
+	    recall = 1.0*true_positives/(true_positives+false_negatives)
+	    f1 = 2.0 * true_positives/(2*true_positives \
+	    	+ false_positives+false_negatives)
+	    print "--------------------"
+	    print "Algorithm Cross Validation: "
+	    print "Precision Score: ", precision
+	    print "Recall Score: ", recall
+	    print "---------------------"
+
+	except:
+	    print "Got a divide by zero when trying out:", clf
+	    print """
+	    Precision or recall may be undefined due to 
+	    a lack of true positive predicitons.
+	    """ 
+cross_validation(clf, my_dataset, features_list)
 test_classifier(clf, my_dataset, features_list)
 
 
@@ -250,6 +311,7 @@ def get_kbest_score():
 	for i in range(len(features_considered)-1):
 	    print features_considered[i+1], ": ", transform.scores_[i]
 
+
 def get_score(clf, dataset, feature_list, return_accuracy = False, 
 	return_precision = False, return_recall = False, return_f1 = False, 
 	folds = 1000):
@@ -296,8 +358,8 @@ def get_score(clf, dataset, feature_list, return_accuracy = False,
 	    accuracy = 1.0*(true_positives + true_negatives)/total_predictions
 	    precision = 1.0*true_positives/(true_positives+false_positives)
 	    recall = 1.0*true_positives/(true_positives+false_negatives)
-	    f1 = 2.0 * true_positives/(2*true_positives \
-	    	+ false_positives+false_negatives)
+	    f1 = 2.0 * true_positives/(
+	    	2*true_positives + false_positives+false_negatives)
 	    result = []
 	    if return_accuracy:
 	    	result.append(accuracy)
@@ -428,3 +490,4 @@ def rfe():
 # al_tuning(features1)
 #al_tuning(features2)
 #al_tuning(features2)
+
